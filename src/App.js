d80e8b60ae4dd0648f4ccea61c58f9abf16491b5
Ben B <benbasuni1@gmail.com>
2024-06-04 14:48:35 -0500

    deleted forms and made it subscribe

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Subscribe from './pages/Subscribe';
-import Forms from './pages/Forms';
 import './App.css';
 
 function App() {
@@ -15,14 +14,12 @@ function App() {
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/subscribe">Subscribe</Link></li>
-            <li><Link to="/forms">Forms</Link></li>
           </ul>
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/subscribe" element={<Subscribe />} />
-          <Route path="/forms" element={<Forms />} />
         </Routes>
       </div>
     </Router>
